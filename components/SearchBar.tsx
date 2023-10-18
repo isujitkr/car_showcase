5433aae30636ddbd3370d7656527bd0ba67a3960
Sujit Kumar <skr87981@gmail.com>
2023-10-19 00:11:57 +0530

    header and footer added

---
+++ components/SearchBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SearchBar = () => {
+    const handleSearch = () => {
+        
+    }
+  return (
+      <form className="searchbar" onSubmit={handleSearch}>
+          
+    </form>
+  )
+}
+
+export default SearchBar;
