7354a32ad9ddbb82f132fcced95cb89c2ae72e04
Sujit Kumar <skr87981@gmail.com>
2023-10-19 22:57:17 +0530

    some changes

--- utils/index.ts
+++ utils/index.ts
@@ -1,15 +1,17 @@
-
+require("dotenv").config();
 export async function fetchCars() {
-    const headers = {
-    "X-RapidAPI-Key": "0f790fa5camsh9b56ad78b9c9b1dp1ca6cfjsn30d7579390b2",
+  const headers = {
+    "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
-    }
-    
-    const response = await fetch(
-        "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars", {
-        headers: headers,
-    });
+  };
 
-    const result = await response.json();
-    return result;
+  const response = await fetch(
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars",
+    {
+      headers: headers,
+    }
+  );
+
+  const result = await response.json();
+  return result;
 }
